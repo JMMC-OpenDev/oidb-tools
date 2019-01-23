6db2faf87394c9b15ed2aae16d648bc5d7f38030
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2019-01-23 18:44:10 +0000

    multiple improvements and fixes

--- syncMirror.sh
+++ syncMirror.sh
@@ -4,6 +4,7 @@ SCRIPT_ROOT=$(dirname $(readlink -f $0))
 source $SCRIPT_ROOT/env.sh
 
 function syncCollection(){
+  installTrap
   if [ -z "$1" ] ; then return ; fi # ignore op without args 
 
   COLLECTION="$1"
@@ -31,37 +32,41 @@ function syncCollection(){
 # Choose med-high or low mode template depending on NB_CHANNELS meta
 #
 function genOIXP(){
+  installTrap
   if [ "$META_NB_CHANNELS" -gt 8 ] ; 
   then 
       OIXP_TMPL=$SCRIPT_ROOT/oidb-template-med_high_res.oixp
   else
       OIXP_TMPL=$SCRIPT_ROOT/oidb-template-low_res.oixp
   fi
-  
   cp $OIXP_TMPL $GRANULE_OIXP
-  # update fields 
-  # TODO fixx all field updates
-  xml ed -L -u "//file/name" -v "$(basename $OIFITS_FILE)" -u "//file/file" -v "$OIFITS_FILE" -u "//target/target" -v "$META_TARGET_NAME" -u "//filter/targetUID" -v "$META_TARGET_NAME" $GRANULE_OIXP
+  xml ed -L -u "//file/name" -v "$(basename $OIFITS_FILE)" -u "//file/file" -v "$OIFITS_FILE" -u "//filter/targetUID" -v "$META_TARGET_NAME" -u "//filter/insModeUID" -v "$META_INSTRUMENT_NAME" -u "//filter/nightID" -v "$METAX_NIGHTID" $GRANULE_OIXP
+  echo -ne "Generate OIXP : $GRANULE_OIXP \r"
 }
 
 #
 #
 #
 function genPNG(){
-  java -Djava.awt.headless=true -jar  $OITOOLS_JAR -png $GRANULE_PNG -mode single -dims 1200,800 -open $GRANULE_OIXP &> ${GRANULE_PNG}.log
+  #installTrap next cmd may return error code
+  if ! java -Djava.awt.headless=true -jar  $OITOOLS_JAR -png $GRANULE_PNG -mode single -dims 1200,800 -open $GRANULE_OIXP &> ${GRANULE_PNG}.log
+  then 
+      echo  "Can't generate PNG : $GRANULE_PNG"
+  fi
 }
 
 #
 # Build xml fragment for png file
 #
 function genDATALINK(){
+  installTrap
   #OIFITS_FILE
   #GRANULE_OIXP
   #GRANULE_PNG
   ACCESS_URL=${DATALINK_FILES_ROOT_URL}/${META_DATA_RIGHTS}/$(basename $GRANULE_PNG)
-  CONTENT_LENGTH=$(stat -c '%s' $GRANULE_PNG)
   if [ -e "$GRANULE_PNG" ]
   then
+      CONTENT_LENGTH=$(stat -c '%s' $GRANULE_PNG)
       echo "<datalink id='${META_ID}'> <access_url>$ACCESS_URL</access_url> <description>Quick plot</description> <content_type>application/png</content_type> <content_length>$CONTENT_LENGTH</content_length></datalink>" > $DATALINK_FILE
   fi
 }
@@ -71,27 +76,43 @@ function genDATALINK(){
 # Create datalink files per granules from a given ofits or skip if already present
 #
 function genDatalinksFromGranule(){
+  installTrap
   if [ -z "$1" ] ; then return ; fi # ignore op without args 
 
   GRANULE_ENV="$1"
   source $GRANULE_ENV # load metadata as META_xxx variables
 
+  if [ "$META_OBS_COLLECTION" = "PIONIER" ]
+  then
+    echo -ne "genDatalinksFromGranule IGNORE $META_OBS_COLLECTION for collection $META_OBS_COLLECTION\r"
+    return
+  else
+    echo -ne "genDatalinksFromGranule $META_OBS_COLLECTION"
+  fi
+
   URL="$(fixUrl ${META_ACCESS_URL} ${SERVER})"
   # define valid names and paths
   NORM_URL=$(urlToFilename $URL)
   OIFITS_FILE="$OIFITS_ROOT_DIR/$NORM_URL"
-  syncFileFromUrl  "$URL" "$OIFITS_FILE"
  
   GRANULE_OIXP="${DATALINK_FILES_ROOT_DIR}/${META_DATA_RIGHTS}/granule_$META_ID.oixp"
   GRANULE_PNG="${GRANULE_OIXP/.oixp/.png}"
   DATALINK_FILE="${DATALINK_FILES_ROOT_DIR}/${META_DATA_RIGHTS}/datalinks_$META_ID.xml"
- #  if [ ! -e "$GRANULE_OIXP" ] ; then genOIXP ; fi
- # if [ ! -e "$GRANULE_PNG" ] ; then genPNG ; fi
- # if [ ! -e "$DATALINK_FILE" ] ; then genDATALINK ; fi
+
+  # Download if not present
+  if [ ! -s "$OIFITS_FILE" ] ; then 
+      # cleanup children files
+      rm $GRANULE_OIXP $GRANULE_PNG $GRANULE_PNG.log $DATALINK_FILE &> /dev/null
+      syncFileFromUrl  "$URL" "$OIFITS_FILE" || return $?
+  fi
+  if [ ! -s "$GRANULE_OIXP" ] ; then genOIXP ; fi
+  if [ ! -s "$GRANULE_PNG" ] ; then genPNG ; fi
+  if [ ! -e "$DATALINK_FILE" ] ; then genDATALINK ; fi
 }
 
 
 function genGranuleFiles(){
+  installTrap
   cd $GRANULE_FILES_ROOT_DIR
   xsltproc $SCRIPT_ROOT/splitGranules.xsl $MIRROR_ROOT/granules.xml
 # local job couldbe mimiced using next command
@@ -102,12 +123,14 @@ function genGranuleFiles(){
 }
 
 function getGranuleFile(){
+  installTrap
   if [ -z "$1" ] ; then return ; fi # ignore op without args 
   GRANULE_ID="$1"
   echo $GRANULE_FILES_ROOT_DIR/granule_${GRANULE_ID}.xml
 }
 
 function syncFileFromUrl(){
+  installTrap
   if [ -z "$1" ] ; then return ; fi # ignore op without args 
   URL="$1"
   MIRROR_FILENAME="$2"
@@ -115,15 +138,12 @@ function syncFileFromUrl(){
   #prepare parent dirs
   mkdirIfMissing "$(dirname $MIRROR_FILENAME)"  
 
-  # Download if not present
-  if [ ! -f "$MIRROR_FILENAME" ] ; then 
-      if ! wget -q $URL -O $MIRROR_FILENAME
-      then 
-          echo "ERROR: can't retrieve $URL into $MIRROR_FILENAME"
-          return 1
-      else 
-          echo "INFO: '$MIRROR_FILENAME' retrieved"
-      fi
+  if ! wget -q $URL -O $MIRROR_FILENAME
+  then 
+      echo "ERROR: can't retrieve $URL into $MIRROR_FILENAME"
+      return 1
+  else 
+      echo "INFO: '$MIRROR_FILENAME' retrieved"
   fi
 }
 
@@ -146,7 +166,7 @@ if ! wget -q $COLLECTIONS_URL -O collections.xml ; then echo "ERROR: Can not ret
 # prepare collection directories 
 echo "- Syncing collections ..."
 xml sel -t -m "//collection" -v "@id" -n collections.xml | while read collection; do syncCollection $collection ; done
-echo "Done"
+echoDone
 
 
 
@@ -157,17 +177,23 @@ GRANULES_ENDPOINT=${SERVER}/restxq/oidb/mirror/granules
 if [ ! -e granules.xml ] 
 then 
   if ! wget $GRANULES_ENDPOINT -O granules.xml ; then echo "ERROR: Can not retrieve granules ($GRANULES_ENDPOINT)... exiting!" ; exit ; fi
-  echo "Done"
+  echoDone
 else
   echo "Skipped"
 fi
 
 # synchronize remote urls onto local mirror
 echo "- Build granule files ..."
-genGranuleFiles
-echo "Done"
+
+if [ "$GRANULE_FILES_ROOT_DIR" -ot "granules.xml" ]
+then
+  genGranuleFiles
+  echoDone
+else 
+  echo "Skipped"
+fi
 
 # generate datalink files from granule files
 echo "- Generate datalink files (this also download the file if not yet present) ..."
 find $GRANULE_FILES_ROOT_DIR -name "*.env" | while read granulefile; do genDatalinksFromGranule $granulefile ; done
-echo "Done"
+echoDone
